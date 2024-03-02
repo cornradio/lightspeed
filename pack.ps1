418cd66419267c688744ecd4fd00bb6c836c429f
Name <kasusaland@gmail.com>
2024-03-02 23:00:03 +0800

    Update pack.ps1

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,5 @@
-pyinstaller --exclude=pandas --exclude=numpy --exclude=libcrypto --exclude=PIL --onefile --icon=assests\icon.ico  lightspeed.py
-# //copy assests to dist
-xcopy README.md dist\ /e /y
+Remove-Item -Path .\dist\* -Recurse -Force
+pyinstaller  --exclude=pyinstaller --exclude=pandas --exclude=numpy --exclude=libcrypto --exclude=PIL --onefile --icon=assests\icon.ico  lightspeed.py
+xcopy  assests\icon.ico dist\assests\ /y
 # //compress \dist\lightspeed to lightspeed.zip using pwsh
-Compress-Archive -Path .\dist\ -DestinationPath .\lightspeed.zip -Force
+Compress-Archive -Path .\dist\ -DestinationPath .\lightspeed_vx.x.zip -Force
