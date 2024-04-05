4e08972bab5e7e6dca86b439153a7c39debe64f9
Name <kasusaland@gmail.com>
2024-04-05 11:50:24 +0800

    add ahkv1 to pack

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,6 @@
 Remove-Item -Path .\dist\* -Recurse -Force
 pyinstaller  --exclude=pyinstaller --exclude=pandas --exclude=numpy --exclude=libcrypto --exclude=PIL --onefile --icon=assests\icon.ico  lightspeed.py
 xcopy  assests\icon.ico dist\assests\ /y
+xcopy  assests\AutoHotkey_1.1.37.02_setup.exe dist\ /y
 # //compress \dist\lightspeed to lightspeed.zip using pwsh
 Compress-Archive -Path .\dist\ -DestinationPath .\lightspeed_vx.x.zip -Force
